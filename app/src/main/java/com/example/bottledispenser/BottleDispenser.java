167b8532a022ec308e2d7491981e7f2bb7d44788
Juho Spoof <juho.spoof@kolumbus.fi>
2021-03-15 21:32:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bottledispenser/BottleDispenser.java
@@ -0,0 +1,99 @@
+package com.example.bottledispenser;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+
+public class BottleDispenser {
+
+    private static BottleDispenser BD = new BottleDispenser();
+
+    /*
+    private BottleDispenser(){
+    }
+    */
+
+    public static BottleDispenser getInstance(){
+        return BD;
+    }
+
+
+    private int bottles;
+
+    // The array for the Bottle-objects
+    //private Bottle[] bottle_array;
+    ArrayList<Bottle> bottle_array = new ArrayList<Bottle>();
+    private int money;
+
+
+
+    public BottleDispenser() {
+        bottles = 5;
+        money = 0;
+
+        for(int i = 0; i<bottles; i++) {
+            Bottle temp = new Bottle();
+            bottle_array.add(temp);
+        }
+
+
+
+    }
+
+    public void addMoney(TextView naytto, SeekBar seekBar) {
+        //int seekValue = seekBar.getProgress();
+        //money += 1;
+        money += seekBar.getProgress();
+        System.out.println("Klink! Added more money!");
+        naytto.setText("Klink! Added more money!");
+    }
+
+    public void buyBottle(TextView naytto) {
+        int i = bottles;
+        if (bottles < 1){
+            System.out.println("Out of bottles!");
+            naytto.setText("Out of bottles!");
+        }
+        else if (money > 1.8){
+            Bottle bArray = bottle_array.get(1);
+            //for(Bottle bArray : bottle_array){
+            bottles -= 1;
+            money   -= bArray.getPrice();
+            System.out.println("KACHUNK! "+ bArray.getName() +" came out of the dispenser!");
+            naytto.setText("KACHUNK! "+ bArray.getName() +" came out of the dispenser!");
+            //}
+        }
+        else{
+            System.out.println("Add money first!");
+            naytto.setText("Add money first!");
+        }
+
+    }
+
+    public void returnMoney(TextView naytto) {
+        money = 0;
+        System.out.println("Klink klink. Money came out!");
+        naytto.setText("Klink klink. Money came out!");
+    }
+
+    public void bottleList(){
+        for(int i=0; i < bottles; i++){
+            Bottle bArray = bottle_array.get(i);
+            int a = i+1;
+            System.out.println((a)+". Name: "+bArray.getName());
+            System.out.println("\t"+"Size: "+bArray.getSize()+"\t"+"Price: "+bArray.getPrice());
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/bottledispenser/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.bottledispenser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button addMoneyButton;
+    Button returnMoneyButton;
+    Button buyBottleButton;
+    TextView naytto;
+    TextView moneyNaytto;
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottleDispenser BD = BottleDispenser.getInstance();
+        //BottleDispenser BD = new BottleDispenser();
+
+        naytto = (TextView) findViewById(R.id.naytto);
+        moneyNaytto = (TextView) findViewById(R.id.moneyNaytto);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        /*seekBar.setMax(2);
+        seekBar.setProgress(1); // default value
+        */
+        addMoneyButton = (Button) findViewById(R.id.addMoneyButton);
+        addMoneyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BD.addMoney(naytto, seekBar);
+            }
+        });
+
+        returnMoneyButton = (Button) findViewById(R.id.returnMoneyButton);
+        returnMoneyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BD.returnMoney(naytto);
+            }
+        });
+
+        buyBottleButton = (Button) findViewById(R.id.buyBottleButton);
+        buyBottleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BD.buyBottle(naytto);
+            }
+        });
+
+
+        /*
+        BD.addMoney();
+        BD.buyBottle();
+        BD.returnMoney();
+        BD.bottleList();
+        */
+    }
+
+
+    //BottleDispenser BD = BottleDispenser.getInstance();
+    //BottleDispenser BD = new BottleDispenser();
+    /*
+    BD.addMoney(View v);
+    BD.buyBottle();
+    BD.returnMoney();
+    BD.bottleList();
+    */
+
+
+
+}
+
